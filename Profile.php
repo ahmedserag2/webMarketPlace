98403636040b92403e6672f81031302228e5ae4e
Ahmed Yehia <ahmedyehia2576@gmail.com>
2020-11-17 13:16:08 +0200

    Main

---
+++ Profile.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    <body>
+    <?php include "index.php" ?>
+    <section class="about section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="single-image">
+              <a href="#"><img src="assets/img/about/shop.jpg"  alt=""></a>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="about-content">
+              <h3>Welcome to eMart</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. U enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dol in reprehenderit in </p>
+              <br>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. U enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dol in reprehenderit in ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dol in reprehenderit in </p>
+              <a href="#" class="btn btn-common btn-more">Read More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- About Us Section End -->
+
+    <!-- Main Ad Section -->
+    <section class="section">
+              <!-- LAYERS 1-->
+              <div class="text-center">
+                <p>You are trying free version</p>
+                <h2>Purchase Full Version to Get All Pages and Features</h2>
+                <p></p>
+                <a class="btn btn-danger" rel="nofollow" href="https://rebrand.ly/purchase-emart">Purchase Now</a>
+              </div>
+    </section>
+    <!-- Main Ad Section End-->
+
+    <!-- Support Section Start -->
+    <div class="support section">
+      <div class="container">
+        <div class="row">
+         <div class="support-inner">
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="row-normal clearfix">
+              <div class="support-info">
+                <div class="info-title">
+                  <i class="icon-plane"></i>
+                  Free Shipping Worldwide
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="row-normal clearfix">
+              <div class="support-info">
+                <div class="info-title">
+                  <i class="icon-earphones-alt"></i>
+                  24/7 Customer Service
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="row-normal clearfix">
+              <div class="support-info">
+                <div class="info-title">
+                  <i class="icon-refresh"></i>
+                  Easy Return Policy
+                </div>
+              </div>
+            </div>
+          </div>
+         </div>
+        </div>
+      </div>
+    </div>
+    <!-- Support Section End -->
+
+    <!-- Start Client Section -->
+    <div class="client section">
+      <div class="container">
+        <div class="row">
+          <div id="client-logo" class="owl-carousel">
+            <div class="client-logo item">
+              <a href="#">
+                <img src="assets/img/clients/logo-01.png" alt="" />
+              </a>
+            </div>
+            <div class="client-logo item">
+              <a href="#">
+                <img src="assets/img/clients/logo-02.png" alt="" />
+              </a>
+            </div>
+            <div class="client-logo item">
+              <a href="#">
+                <img src="assets/img/clients/logo-03.png" alt="" />
+              </a>
+            </div>
+            <div class="client-logo item">
+              <a href="#">
+                <img src="assets/img/clients/logo-04.png" alt="" />
+              </a>
+            </div>
+            <div class="client-logo item">
+              <a href="#">
+                <img src="assets/img/clients/logo-05.png" alt="" />
+              </a>
+            </div>
+            <div class="client-logo item">
+              <a href="#">
+                <img src="assets/img/clients/logo-06.png" alt="" />
+              </a>
+            </div>
+            <div class="client-logo item">
+              <a href="#">
+                <img src="assets/img/clients/logo-07.png" alt="" />
+              </a>
+            </div>
+            <div class="client-logo item">
+              <a href="#">
+                <img src="assets/img/clients/logo-08.png" alt="" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+        
+        
+    </body>
+</head>
+</html>
