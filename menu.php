ac41f13041c556c928ce84d7b9f5b5dfd8b6887c
kikosvg <74558972+kikosvg@users.noreply.github.com>
2021-02-15 22:18:32 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -6,12 +6,12 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-  <link rel="stylesheet" href="../CSS/style.css" type="text/css">
+  <link rel="stylesheet" href="CSS/style.css" type="text/css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="../CSS/ProductPage.css" type="text/css"> 
-  <link rel="stylesheet" href="../CSS/bootstrap.min.css" type="text/css"> 
+  <link rel="stylesheet" href="CSS/ProductPage.css" type="text/css"> 
+  <link rel="stylesheet" href="CSS/bootstrap.min.css" type="text/css"> 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="../CSS/bootstrap.css" type="text/css">      
+    <link rel="stylesheet" href="CSS/bootstrap.css" type="text/css">      
 
 
 
